8504e16fd8813efe9d28ff0805700094d24afd91
Vanshhh001 <144903579+Vanshhh001@users.noreply.github.com>
2026-01-03 02:24:17 +0530

    how we login

--- app/src/main/java/com/example/vschat/splash.java
+++ app/src/main/java/com/example/vschat/splash.java
@@ -19,10 +19,10 @@ public class splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, MainActivity.class);
+                Intent intent = new Intent(splash.this,registration.class);
                 startActivity(intent);
                 finish();
             }
-        },40000);
+        },4000);
     }
 }
