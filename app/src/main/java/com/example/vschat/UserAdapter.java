e8e5d3c742c0bfeaa6ca6196ff009b98f1fd7e6c
Vanshhh001 <144903579+Vanshhh001@users.noreply.github.com>
2026-01-17 06:10:01 +0530

    app icon

--- app/src/main/java/com/example/vschat/UserAdapter.java
+++ app/src/main/java/com/example/vschat/UserAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -35,7 +36,16 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.viewholder> {
     public void onBindViewHolder(@NonNull UserAdapter.viewholder holder, int position) {
 
 
+
         Users users = usersArrayList.get(position);
+
+        if(FirebaseAuth.getInstance().getCurrentUser().getUid().equals(users.getUserId())){
+            holder.itemView.setVisibility(View.GONE);
+
+        }
+
+
+
         holder.username.setText(users.userName);
         holder.userstatus.setText(users.status);
         Picasso.get().load(users.profilepic).into(holder.userimg);
